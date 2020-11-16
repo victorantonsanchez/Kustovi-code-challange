66e859f3f066f8fca47eaf2da769f28727e36461
Víctor Antón <victoranton@Pachanga-2.local>
2020-11-16 15:37:57 +0100

    Reset credentials Firebase

--- src/config.js
+++ src/config.js
@@ -11,16 +11,16 @@ const config = {
     },
     "providers": {
         "firebase": {
-            "type": "service_account",
-            "project_id": "kustovi-code-challange",
-            "private_key_id": "a161011b4eba422e3c8e82dfb812ee12f8d71dba",
-            "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDVJQmPqEHDmtKq\nERFxakj8Mq3nTaITSFZGrVdR/mJsL6Ra3vy2CVGbD31BLKbZsf09XJOx5zIXLxj4\nskhmKAaTeZhXTcmF6Qxm1R6Cra+dg9imIFSowcwAnEzM/98a7Unny6jiDMwJ6Ht7\nqUdlmPFAvITwXXI5oF9kf+3YmUC83VGLCkNQvN4RvuB25kbaBYWFaVPCYxEDS/LQ\nWgFUO2PBj93FG3eVEDNBFGC3Caeuc6etEmhzx3/5oNsBLJDBP/vDDKXNJwBwj/6E\nULnqyukx8f3n6ERdmd32yETbIackCS+AG0cLBgPAOgRsxPCwZS9A1UnvqA3SLOAi\n5bUThfstAgMBAAECggEAL0IQwmIVQLwrYMs7gZjF+Wr4LWyEVr+nZ3DIB1U+d8vB\nZmeCAw+CYT24MRLoJKTm5+vGkH+ZFtRZtkx7KnFN8fVYHmVQHQl8tzEiHGnLogJJ\nbx4Ef01GwMzm514FFtiNViBM/9/D/p9l/5dbDe7w8MBLvmqNqLLC68rEjs198dG9\nsvsuT3dA+0Y5eL6skgX1pVrw1A0PcjXWSBQ3G+soZGq5YblL9gZJ3hlg2EU3VUIJ\naYHC+uDORT1Ph24t5dqZxpAs/jHjdcjC+77RkaVNO5kZ7ll0GePjfp3MTjUneMyc\nghITGwqdaiEU4VucMtr2ZWWc1ybDKtyUcWJ7c0bSeQKBgQD0ILY031eXs3h3UKEO\n2yETuAHWP3dIXSrVFThzuV0g0RIWmbm8S5ZZl27EjHP+WFkYIVDNMp+Zub9ordyS\ntRO5qx9KZZ0vaS2niS/EA4tPTCOSUQIaU0OjNzNLvq3e/RqEPEDeg/q1JdaTGY2I\nJ+sgd+1mh5JW3EX1MfIbGYXzqQKBgQDfgplXlcnY2d6BX+DIaOgxtAY29ybvhbQE\nmuUaZlTj0u5VO0mZyja3DxKd50rVGFRGD68m6/E5bURhCuLqK2+K75HtrbiSSw39\nYq5Q4L7O7LYGA8v7Ce+rX+GYJaLiYaRmbdbfchjcPdeXotTcs38j9Z/C3uJiTOYY\n+QfY5N395QKBgQCHgXTBqkq7aKHwUG/liBo3EMbsPjQelaBFwE6iZXxcz8+jfdaL\nvsUIm7afIHrt7eoeK7fm/jd6o5CsMP0i7aGySFrI99gkzdPKj6pUucbk0BVajN6G\n8Dq16NjvLTrjcEf8qFk1abqKPCN7aQ6XSdkIH6lFZPEEh6XJMNz6z9L7aQKBgQDa\nVwUfF1dP/D0tWxEGFd1FuJwdk8C4WX9qxDmsWWzyBbsHDdyvXGeo2hOQp0Jl+tdQ\nfb/XoMNMGx6iQfU7VmUnbzK54Wudla476etpXLNpBromFA1u1d0IJ6kEbTe6ODJW\nUVSHgTkh5VO3cLzKQJqh8S85OWwjJQXePV/aFHF7tQKBgDCD4CYYR1CAUanQ9PjG\n1TSs0c0j6gsPJ6BfoZFr6thpUwGngzwAnCx5ZG0nAQuApoGcAPDMcZh8tbv13DTa\nKqF833JlkGl8LSawC6wpuDIqQc4/Nria4WNCIUakroL76gF0VEeZPrQ6R/XenR06\n6JS9lWa+KLKQAloFQ0Wwk3mq\n-----END PRIVATE KEY-----\n",
-            "client_email": "firebase-adminsdk-twf31@kustovi-code-challange.iam.gserviceaccount.com",
-            "client_id": "111110674934867368811",
-            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-            "token_uri": "https://oauth2.googleapis.com/token",
-            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-twf31%40kustovi-code-challange.iam.gserviceaccount.com"
+            "type": "",
+            "project_id": "",
+            "private_key_id": "",
+            "private_key": "",
+            "client_email": "",
+            "client_id": "",
+            "auth_uri": "",
+            "token_uri": "",
+            "auth_provider_x509_cert_url": "",
+            "client_x509_cert_url": ""
         }
     }
 }
